21420240e0b70604a21bc515a2d43540ec0e9f6f
Korhan Mutludoğan <korhanmd@gmail.com>
2021-03-31 15:19:39 +0300

    Add test for queue with assertions

---
+++ test_2.py
@@ -0,0 +1,56 @@
+# CORRECT SPECIFICATION:
+#
+# the Queue class provides a fixed-size FIFO queue of integers
+#
+# the constructor takes a single parameter: an integer > 0 that
+# is the maximum number of elements the queue can hold.
+#
+# empty() returns True if and only if the queue currently 
+# holds no elements, and False otherwise.
+#
+# full() returns True if and only if the queue cannot hold 
+# any more elements, and False otherwise.
+#
+# enqueue(i) attempts to put the integer i into the queue; it returns
+# True if successful and False if the queue is full.
+#
+# dequeue() removes an integer from the queue and returns it,
+# or else returns None if the queue is empty.
+#
+# Example:
+# q = Queue(1)
+# is_empty = q.empty()
+# succeeded = q.enqueue(10)
+# is_full = q.full()
+# value = q.dequeue()
+#
+# 1. Should create a Queue q that can only hold 1 element
+# 2. Should then check whether q is empty, which should return True
+# 3. Should attempt to put 10 into the queue, and return True
+# 4. Should check whether q is now full, which should return True
+# 5. Should attempt to dequeue and put the result into value, which 
+#    should be 10
+
+from queue import *
+
+def test():
+    ###Your code here.
+    q = Queue(1)
+    assert q.max == 1
+    
+    is_empty = q.empty()
+    assert is_empty
+    
+    succeeded = q.enqueue(10)
+    assert succeeded
+    
+    is_full = q.full()
+    assert is_full
+    
+    value = q.dequeue()
+    assert value == 10
+    
+    print "Test Finished"
+    
+    
+test()
